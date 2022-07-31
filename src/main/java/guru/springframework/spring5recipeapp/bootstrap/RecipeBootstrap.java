8964a14470f5a4565f020ed56d2da1c3944f2a8a
Alexander Reineck <areineck@ohgiftcard.com>
2022-07-31 13:30:37 -0300

    Display List of Recipes

---
+++ src/main/java/guru/springframework/spring5recipeapp/bootstrap/RecipeBootstrap.java
@@ -0,0 +1,148 @@
+package guru.springframework.spring5recipeapp.bootstrap;
+
+import guru.springframework.spring5recipeapp.domain.*;
+import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
+import guru.springframework.spring5recipeapp.repositories.RecipeRepository;
+import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final CategoryRepository categoryRepository;
+    private final RecipeRepository recipeRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public RecipeBootstrap(CategoryRepository categoryRepository, RecipeRepository recipeRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.recipeRepository = recipeRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        recipeRepository.saveAll(getRecipes());
+    }
+
+    private List<Recipe> getRecipes() {
+
+        List <Recipe> recipes = new ArrayList<>(2);
+
+        //get UOMs
+        Optional<UnitOfMeasure> eachUomOptional = unitOfMeasureRepository.findByDescription("Each");
+
+        if(eachUomOptional.isEmpty()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> tableSpoonUomOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        if(tableSpoonUomOptional.isEmpty()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> teaSpoonUomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        if(teaSpoonUomOptional.isEmpty()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> dashUomOptional = unitOfMeasureRepository.findByDescription("Dash");
+
+        if(dashUomOptional.isEmpty()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+
+        Optional<UnitOfMeasure> pintUomOptional = unitOfMeasureRepository.findByDescription("Pint");
+
+        if(pintUomOptional.isEmpty()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        Optional<UnitOfMeasure> cupsUomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        if(cupsUomOptional.isEmpty()) {
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+
+        //get Optionals
+        UnitOfMeasure eachUom = eachUomOptional.get();
+        UnitOfMeasure tableSpoonUom = tableSpoonUomOptional.get();
+        UnitOfMeasure teaSpoonUom = teaSpoonUomOptional.get();
+
+        //get Categories
+        Optional<Category> americanCategoryOptional = categoryRepository.findByDescription("American");
+
+        if(americanCategoryOptional.isEmpty()) {
+            throw new RuntimeException("Expected Category Not Found");
+        }
+
+        Optional<Category> mexicanCategoryOptional = categoryRepository.findByDescription("Mexican");
+
+        if(mexicanCategoryOptional.isEmpty()) {
+            throw new RuntimeException("Expecter Category Not Found");
+        }
+
+        Category americanCategory = americanCategoryOptional.get();
+        Category mexicanCategory = mexicanCategoryOptional.get();
+
+        //Yummy Guac
+        Recipe guacRecipe = new Recipe();
+        guacRecipe.setDescription("Perfect Guacamole");
+        guacRecipe.setPrepTime(10);
+        guacRecipe.setCookTime(0);
+        guacRecipe.setDifficulty(Difficulty.EASY);
+        guacRecipe.setDirections("1 Cut avocado, Cut the avocados in half. Remove the pit. Score the inside of the avocado with a blunt knife and scoop out the flesh with a spoon. Place in a bowl... ");
+
+        Notes guacNotes = new Notes();
+        guacNotes.setRecipeNotes("For a very quick guacamlole just take a 1/4 cup of salsa...");
+
+        guacNotes.setRecipe(guacRecipe);
+        guacRecipe.setNotes(guacNotes);
+
+        guacRecipe.getIngredients().add(new Ingredient("Ripe avocados", new BigDecimal(2), eachUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Kosher salt", new BigDecimal(".5"), teaSpoonUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Fresh lime", new BigDecimal(2), tableSpoonUom, guacRecipe));
+
+        guacRecipe.getCategories().add(americanCategory);
+        guacRecipe.getCategories().add(mexicanCategory);
+
+        recipes.add(guacRecipe);
+
+        // Yummy Tacos
+        Recipe tacosRecipe = new Recipe();
+        tacosRecipe.setDescription("Spicy Grilled Chicken Taco");
+        tacosRecipe.setCookTime(9);
+        tacosRecipe.setPrepTime(20);
+        tacosRecipe.setDifficulty(Difficulty.MODERATE);
+
+        tacosRecipe.setDirections("1 Prepare a gas or charcoal grill for medium-high...");
+
+        Notes tacoNotes = new Notes();
+        tacoNotes.setRecipeNotes("We have family motto...");
+
+        tacoNotes.setRecipe(tacosRecipe);
+        tacosRecipe.setNotes(tacoNotes);
+
+        tacosRecipe.getIngredients().add(new Ingredient("Ancho Chili Powder", new BigDecimal(2), tableSpoonUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("Dried Oregano", new BigDecimal(1), teaSpoonUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("Dried Cumin", new BigDecimal(1), teaSpoonUom, tacosRecipe));
+
+        tacosRecipe.getCategories().add(americanCategory);
+        tacosRecipe.getCategories().add(mexicanCategory);
+
+        recipes.add(tacosRecipe);
+
+        return recipes;
+    }
+
+
+}
--- src/main/java/guru/springframework/spring5recipeapp/controllers/indexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/indexController.java
@@ -4,7 +4,9 @@ import guru.springframework.spring5recipeapp.domain.Category;
 import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
 import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import guru.springframework.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
@@ -12,20 +14,17 @@ import java.util.Optional;
 @Controller
 public class indexController {
 
-    private final CategoryRepository categoryRepository;
-    private final UnitOfMeasureRepository unitOfMeasureRepository;
+    private final RecipeService recipeService;
 
-    public indexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    public indexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(){
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
-        System.out.println("Cat Id is: " + categoryOptional.get().getId());
-        System.out.println("UOM Id id: " + unitOfMeasureOptional.get().getId());
+    public String getIndexPage(Model model){
+
+        model.addAttribute("recipes", recipeService.getRecipes());
+
         return "index";
     }
 
