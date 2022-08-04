ea1bb18c45735a9ede9ba09356312b150ae14250
Alexander Reineck <areineck@ohgiftcard.com>
2022-08-04 16:28:03 -0300

    Maven Failsafe Plugin (Don't work and replaced by jUnit Vintage & Harmcrest

--- src/test/java/guru/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/guru/springframework/spring5recipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -4,15 +4,13 @@ import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
