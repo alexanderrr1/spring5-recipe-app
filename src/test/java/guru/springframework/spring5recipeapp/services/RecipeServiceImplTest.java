d072de0b325bd449907b8a09b33aa751f8777cfa
Alexander Reineck <areineck@ohgiftcard.com>
2022-08-06 08:41:38 -0300

    Display Recipe by ID

--- src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -8,9 +8,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.*;
 
 public class RecipeServiceImplTest {
@@ -27,16 +29,24 @@ public class RecipeServiceImplTest {
     }
 
     @Test
-    public void getRecipes() {
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+        Recipe recipeReturned = recipeService.findById(1L);
+        assertNotNull("Null Recipe Returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
 
+    @Test
+    public void getRecipesTest() {
         Recipe recipe = new Recipe();
         HashSet<Recipe> recipesData = new HashSet<>();
         recipesData.add(recipe);
-
         when(recipeRepository.findAll()).thenReturn(recipesData);
-
         Set<Recipe> recipes = recipeService.getRecipes();
-
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
 
